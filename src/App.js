3d35e25acb52c39530df93f0001305c8b7df84f2
nayak16 <aatishnayak1a@gmail.com>
2017-11-07 19:29:05 -0800

    Timers with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,153 +1,226 @@
+import { Map } from 'immutable';
+
 import React, { Component } from 'react';
 import './App.css';
+import { 
+  Button, ButtonGroup, Col, FormGroup, 
+  ControlLabel, Panel, FormControl, HelpBlock, 
+  Grid, Row, Glyphicon, PageHeader 
+} from 'react-bootstrap';
 
-
-class FilterSection extends Component {
-  render() {
-    return (
-      <form>
-        <input type="text" onInput={e => this.props.onInput(e.target.value)} placeholder="Search..."></input>
-        <p>
-        <label><input name="Checkbox1" type="checkbox" onClick={e => this.props.onCheckboxChange(e.target.checked)}/>In Stock Only</label>
-        </p>
-      </form>
-    )
-  }
-}
-
-class CategoryHeader extends Component {
-  render() {
-    return (
-      <tr>
-        <th colSpan="2">
-          {this.props.category}
-        </th>
-      </tr>
-    )
-  }
-}
-
-class ProductRow extends Component {
-  render() {
-    const product = this.props.product
-    const name = product.stocked ? 
-      product.name :
-      <span style={{color: 'red'}}>
-        {product.name}
-      </span>
-
-    return (
-      <tr>
-        <td>
-          {name}
-        </td>
-        <td>
-          {product.price}
-        </td>
-      </tr>
-    )
-  }
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
 }
 
 
-class ProductTable extends Component {
-  render() {
-    var headers = {}
-
-    this.props.products.forEach((product) => {
-      var filter = this.props.filterText
-      console.log(this.props.inStockOnly)
-      if (!this.props.inStockOnly || (this.props.inStockOnly && product.stocked)) {
-        if (filter == '' || product.name.indexOf(filter) != -1) {
-          if (!(product.category in headers)) {
-            headers[product.category] = []
-            headers[product.category].push(
-              <CategoryHeader category={product.category} />
-            )
-          }
-          headers[product.category].push(
-            <ProductRow product={product} />
-          )
-        }
-      }
-    });
-    var rows = []
-    for (let cat in headers) {
-      headers[cat].forEach((row) => {
-        rows.push(row)
-      })
-    }
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    )
-  }
-}
-
-class FilterableProductTable extends Component {
+class TimerDisplay extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+  }
+  render() {
+    var seconds = this.props.seconds;
+    var minutes = Math.floor(seconds / 60);
+    var truncSeconds = seconds - (60 * minutes)
+    return (
+      <h5>{minutes}:{truncSeconds}</h5>
+    )
+  }
+}
+
+
+class Timer extends Component {
+  constructor() {
+    super()
     this.state = {
-      filterText: '',
-      inStockOnly: false
+      started: false,
+      seconds: 0
     }
-    this._onInputChange = this.onInputChange.bind(this)
-    this._onCheckboxChange = this.onCheckboxChange.bind(this)
+    this.setIntervalId = 0
+    this.handleDelete = this.handleDelete.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
   }
 
-  onInputChange(newValue) {
+  handleDelete(e) {
+    e.preventDefault();
+    this.stopTimer()
+    this.props.delete(this.props.id);
+  }
+
+  tick() {
     this.setState({
-      filterText: newValue
+        seconds: this.state.seconds + 1
     })
   }
 
-  onCheckboxChange(newValue) {
+  startTimer() {
+    this.stopTimer()
+    this.setIntervalId = setInterval(this.tick.bind(this), 100)
     this.setState({
-      inStockOnly: newValue
+      started: true
+    })
+  }
+
+  stopTimer() {
+    clearInterval(this.setIntervalId)
+    this.setState({
+      started: false
     })
   }
 
   render() {
-    return (<div>
-      <FilterSection 
-        filterText={this.state.filterText}
-        inStockOnly={this.state.inStockOnly}
-        onInput={this._onInputChange}
-        onCheckboxChange={this._onCheckboxChange}/>
-
-      <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}></ProductTable>
-      </div>
+    var started = this.state.started;
+    const header = (<div><h3>{this.props.title}<small style={{float: "right"}} className='float-md-right'><a href="#" onClick={this.handleDelete}>x</a></small></h3></div>);
+    return (
+      <Panel header={header}>
+        <Grid>
+          <Row className="">
+            <Col md={4}><TimerDisplay seconds={this.state.seconds}></TimerDisplay></Col>
+          </Row>
+          <Row className="">
+            <Col md={12}>
+              <ButtonGroup>
+                {started ? 
+                  <Button onClick={this.stopTimer} bsStyle="danger">Stop</Button> :               
+                  <Button onClick={this.startTimer} bsStyle="success">Start</Button>
+                }
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </Grid>
+      </Panel>
     )
   }
 
 }
+
+class AddTimerForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      onForm: false,
+      titleText: ""
+    }
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({
+      onForm: !this.state.onForm
+    })
+  }
+
+  handleTitleChange(event){
+    this.setState({
+      titleText: event.target.value
+    })
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    this.toggleForm();
+    this.props.formSubmit(this.state.titleText);
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row className="">
+          <Col md={4}>
+            {
+              this.state.onForm ? 
+              (
+                <form>
+                  <FieldGroup
+                    id="titleInput"
+                    type="text"
+                    label="Timer Title"
+                    placeholder="Enter timer title"
+                    onChange={this.handleTitleChange}
+                  />
+                  <Button type="submit" onClick={this.handleFormSubmit}>
+                    Submit
+                  </Button>
+                  <Button onClick={this.toggleForm}>
+                    Cancel
+                  </Button>
+                </form>
+                ) : (
+                <Button onClick={this.toggleForm}>+</Button>
+              )
+            }
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+
+}
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      serverProducts: []
+      timers: Map({}),
+      timerId: 0
     }
+
+    this.createTimer = this.createTimer.bind(this);
+    this.deleteTimer = this.deleteTimer.bind(this);
   }
 
   componentDidMount() {
-    fetch("/products.json")
-      .then(res => res.json())
-      .then(products => {
-        return this.setState({ serverProducts: products.products })
-      })
+    this.createTimer("Default Timer");
+  }
+
+  createTimer(title) {
+    console.log(title);
+    var timers = this.state.timers;
+    var timerId = this.state.timerId;
+    var updatedTimers = timers.set(timerId, <Timer title={title} id={timerId} delete={this.deleteTimer}/>)
+    this.setState({
+      timers: updatedTimers,
+      timerId: timerId + 1,
+    })
+  }
+
+  deleteTimer(id) {
+    var deleted = this.state.timers.delete(id);
+    this.setState({
+      timers: deleted
+    })
   }
 
   render() {
+    var timers = this.state.timers;
+
+    var displayTimers = timers.toArray()
+    console.log(displayTimers);
     return (
-      <FilterableProductTable products={this.state.serverProducts} />
+      <div>
+        <PageHeader>Timers List<small> by Aatish Nayak</small></PageHeader>
+        <Grid>
+          {displayTimers.map(timer =>
+              <Row>
+                <Col md={4}>{timer}</Col>
+              </Row>
+          )}
+           <Row>
+            <Col md={4}>
+              <AddTimerForm formSubmit={this.createTimer}/>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
